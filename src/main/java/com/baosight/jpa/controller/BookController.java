3154d7b3dad78e9ee53ecc6353bb4dd8de68cb3f
ZYB <1755103832@qq.com>
2020-11-23 17:20:04 +0800

    two commit

--- src/main/java/com/baosight/jpa/controller/BookController.java
+++ src/main/java/com/baosight/jpa/controller/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class BookController {
@@ -17,6 +19,16 @@ public class BookController {
     @GetMapping("/all")
     public List<Book> query1() {
         return bookService.queryAll();
+    }
+
+    @GetMapping("/all2")
+    public Map<String, Object> query2() {
+        List<Book> bookList = bookService.queryAll();
+        Map<String, Object> map = new ConcurrentHashMap<>();
+        if (null != bookList && bookList.size() > 0) {
+            map.put("bookList", bookList);
+        }
+        return map;
     }
 
 }
